1e06835592c3861b798d1a1129aa47f89404b6bb
falconblaze <ahmedmusawon@gmail.com>
2021-12-19 18:32:28 +0600

    add redux to class based component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,47 @@
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 
 import "./header.scss";
 
-const Header = () => {
-  const dispatch = useDispatch();
-  const authenticated = useSelector(
-    (state) => state.authentication.isAuthenticated
-  );
+class Header extends React.Component {
+  logoutHandler() {
+    this.props.logout();
+  }
 
-  const logoutHandler = () => {
-    dispatch({ type: "logout" });
+  render() {
+    return (
+      <header className="header">
+        <h1>Redux Auth</h1>
+        {this.props.authenticated && (
+          <nav>
+            <ul>
+              <li>
+                <a href="/">My Products</a>
+              </li>
+              <li>
+                <a href="/">My Sales</a>
+              </li>
+              <li>
+                <button onClick={this.logoutHandler.bind(this)}>Logout</button>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    authenticated: state.authentication.isAuthenticated,
   };
-
-  return (
-    <header className="header">
-      <h1>Redux Auth</h1>
-      {authenticated && (
-        <nav>
-          <ul>
-            <li>
-              <a href="/">My Products</a>
-            </li>
-            <li>
-              <a href="/">My Sales</a>
-            </li>
-            <li>
-              <button onClick={logoutHandler}>Logout</button>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </header>
-  );
 };
 
-export default Header;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch({ type: "logout" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
